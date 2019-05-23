cb06f03b7192dff76098bfc92c3068fb51fdc68e
Daniel Hueholt <dmhuehol@ncsu.edu>
2019-05-23 17:52:34 -0400

    5/23/2019 update mp4maker
    These versions were written outside of the Github domain

--- mp4maker_4panel.m
+++ mp4maker_4panel.m
@@ -1,11 +1,11 @@
-%%mp4maker
+%%mp4maker_4panel
     %"Function" which saves an mp4 to an input path from images in given
     %directories. For now, requires a lot of personal intervention to locate
     %the data paths and specified azimuths.
     %savePath  - Where the data live. e.g. savePath = 'C:\me\myData\myFrames\'; 
     %
-    %Version Date: 4/20/2019
-    %Last major revision: 2/8/2019
+    %Version Date: 4/5/2019
+    %Last major revision: 4/4/2019
     %Written by: Daniel Hueholt
     %North Carolina State University
     %Undergraduate Research Assistant at Environment Analytics
@@ -21,11 +21,11 @@
         %everything, 'ncp' plots with ncp, 6 panel plots minus derived
         %fields
     
-function [] = mp4maker(savePath)
+function [] = mp4maker_4panel(savePath)
 
 disp('In progress!')
-writerObj = VideoWriter([savePath '20181030_azi271_8panel'],'MPEG-4');
-%format: PPI_YYYYMMDD_8panel_angDEG YYYYMMDD_aziDEG_8panel
+writerObj = VideoWriter([savePath '20181030_azi271_VSWZVDIV'],'MPEG-4'); 
+%format: PPI_YYYYMMDD_DATA_angDEG YYYYMMDD_aziDEG_DATA_RxC
 writerObj.FrameRate = 1; %in frames per second; customize to your data
 open(writerObj);
 %writerObj4 = VideoWriter([savePath '20181030_PPI_4panel'],'MPEG-4');
@@ -33,28 +33,30 @@ open(writerObj);
 %open(writerObj4);
 
 % Combined images have V, SW, Z, snow rate, Zdr, rhoHV, PhiDP, and Kdp in that order
-% NOTE: Paths MUST have a trailing /
 vpath = '/Volumes/Andrew/radar output/CSU-CHILL/20181030/X/RHI/Sweep 0/V_dealiased/';
 %v_aliased_path = '/Volumes/EMTECC450/loop/final_checks/Sweep 1/V_folded/';
 zpath = '/Volumes/Andrew/radar output/CSU-CHILL/20181030/X/RHI/Sweep 0/Z/';
 swpath = '/Volumes/Andrew/radar output/CSU-CHILL/20181030/X/RHI/Sweep 0/SW/';
 zdrpath = '/Volumes/Andrew/radar output/CSU-CHILL/20181030/X/RHI/Sweep 0/Zdr/';
-snowratepath = '/Volumes/Andrew/radar output/CSU-CHILL/20181030/X/RHI/Sweep 0/snow_rate/';
-rhohvpath = '/Volumes/Andrew/radar output/CSU-CHILL/20181030/X/RHI/Sweep 0/rhoHV/';
-phidppath = '/Volumes/Andrew/radar output/CSU-CHILL/20181030/X/RHI/Sweep 0/PhiDP/';
-kdppath = '/Volumes/Andrew/radar output/CSU-CHILL/20181030/X/RHI/Sweep 0/Kdp/';
+vdivpath = '/Volumes/Andrew/radar output/CSU-CHILL/20181030/X/RHI/Sweep 0/vdiv/';
+%snowratepath = '/Volumes/EMTECC450/20130409/Sweep 0/snow_rate/';
+%rhohvpath = '/Volumes/EMTECC450/20130409/Sweep 0/rhoHV/';
+%phidppath = '/Volumes/EMTECC450/20130409/Sweep 0/PhiDP/';
+%kdppath = '/Volumes/EMTECC450/20130409/Sweep 0/Kdp/';
 %ncppath = '/Volumes/EMTECC450/loop/final_checks/Sweep 1/ncp/';
 
 aziString = '*azi27*'; %aziNUM, usually 3-digits but whatever is necessary to uniquely define
+%angNUM for PPI, or some string that appears in all images if all are useful (YYYY for example)
 frameListV = dir([vpath aziString '.png']);%make the wildcard strings that work for you
 %frameListVfold = dir([v_aliased_path '*.png']);
 frameListZ = dir([zpath aziString '.png']);
 frameListSW = dir([swpath aziString '.png']);
 frameListZdr = dir([zdrpath aziString '.png']);
-frameListSnowRate = dir([snowratepath aziString '.png']);
-frameListRhohv = dir([rhohvpath aziString '.png']);
-frameListPhiDP = dir([phidppath aziString '.png']);
-frameListKdp = dir([kdppath aziString '.png']);
+frameListVdiv = dir([vdivpath aziString '.png']);
+%frameListSnowRate = dir([snowratepath aziString '.png']);
+%frameListRhohv = dir([rhohvpath aziString '.png']);
+%frameListPhiDP = dir([phidppath aziString '.png']);
+%frameListKdp = dir([kdppath aziString '.png']);
 %frameListNCP = dir([ncppath '*.png']);
 
 for ii=1:length(frameListZ)-1
@@ -63,17 +65,19 @@ for ii=1:length(frameListZ)-1
     Z = imread([zpath frameListZ(ii).name]);
     SW = imread([swpath frameListSW(ii).name]);
     Zdr = imread([zdrpath frameListZdr(ii).name]);
-    SnowRate = imread([snowratepath frameListSnowRate(ii).name]);
-    rhoHV = imread([rhohvpath frameListRhohv(ii).name]);
-    PhiDP = imread([phidppath frameListPhiDP(ii).name]);
-    Kdp = imread([kdppath frameListKdp(ii).name]);
+    vdiv = imread([vdivpath frameListVdiv(ii).name]);
+    %SnowRate = imread([snowratepath frameListSnowRate(ii).name]);
+    %rhoHV = imread([rhohvpath frameListRhohv(ii).name]);
+    %PhiDP = imread([phidppath frameListPhiDP(ii).name]);
+    %Kdp = imread([kdppath frameListKdp(ii).name]);
     %NCP = imread([ncppath frameListNCP(ii).name]);
     
     %[appropriateR,appropriateC,appropriate3] = size(Zdr);
     %blankMat = zeros(appropriateR,appropriateC,appropriate3);
     
-    concatImage = [V,SW;Z,SnowRate;Zdr,rhoHV;PhiDP,Kdp]; %8 panel RHI
-    %concatImage = [V,SW,Z,SnowRate;Zdr,PhiDP,Kdp,rhoHV]; %8 panel PPI
+    %concatImage = [V,SW;Z,vdiv]; %4 panel RHI 2x2
+    concatImage = [V;SW;Z;vdiv]; %4 panel RHI 4x1
+    %concatImage = [Z]; %1 panel PPI
     %concatImage4 = [V,SW;Z,SnowRate]; %4 panel PPI
     %smallerConcat = imresize(concatImage,[2160 4096]); %y,x for some reason??
     %smallerConcat = concatImage; %test
